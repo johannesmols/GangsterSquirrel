69dda6a95c5cba339e88e6684d6f26ba02e038a9
Johannes Mols <johannes.mols@googlemail.com>
2017-10-23 10:54:23 +0200

    Added placeholder background music and sound effect as example

--- core/src/itcom/gangstersquirrel/Screens/PlayScreen.java
+++ core/src/itcom/gangstersquirrel/Screens/PlayScreen.java
@@ -2,6 +2,8 @@ package itcom.gangstersquirrel.Screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -22,16 +24,27 @@ public class PlayScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer renderer;
 
+    private Sound dropSoundReplaceLater;
+    private Music rainMusicReplaceLater;
+
     public PlayScreen(MainGameClass game) {
         this.game = game;
 
+        // Create a new game camera and set the viewport, divide by the pixels per meter constant for scaling
         camera = new OrthographicCamera();
         viewport = new FitViewport(MainGameClass.WIDTH / MainGameClass.PPM, MainGameClass.HEIGHT / MainGameClass.PPM, camera);
 
+        // Load the first map from Tiles
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("level_1.tmx");
         renderer = new OrthogonalTiledMapRenderer(map, 1 / MainGameClass.PPM);
         camera.position.set(viewport.getWorldWidth() / 2, viewport.getWorldHeight() / 2, 0);
+
+        //Load the template sound effect and the template background music and play immediately
+        dropSoundReplaceLater = Gdx.audio.newSound(Gdx.files.internal("audio/waterdrop_replace_later.wav"));
+        rainMusicReplaceLater = Gdx.audio.newMusic(Gdx.files.internal("audio/rain_replace_later.mp3"));
+        rainMusicReplaceLater.setLooping(true);
+        rainMusicReplaceLater.play();
     }
 
     @Override
@@ -89,6 +102,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        rainMusicReplaceLater.dispose();
+        dropSoundReplaceLater.dispose();
     }
 }
