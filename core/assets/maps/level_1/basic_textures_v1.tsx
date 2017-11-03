b83da862048e1acb19ab7bf99677926d25d63194
Johannes Mols <johannes.mols@googlemail.com>
2017-11-03 16:14:07 +0100

    Updated level 1 map with some of our new textures

---
+++ core/assets/maps/level_1/basic_textures_v1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="basic_textures_v1" tilewidth="16" tileheight="16" tilecount="14" columns="2">
+ <image source="basic_textures_v1.png" width="32" height="112"/>
+</tileset>
