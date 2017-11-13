4dc144826fb1d90036078fab7b87c57b9690f429
Johannes Mols <johannes.mols@googlemail.com>
2017-11-13 12:24:41 +0100

    Added some nicer textures to the map, still needs to replace all the dirt textures

---
+++ core/assets/maps/level_1/grass_dirt.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="grass_dirt_tileset" tilewidth="16" tileheight="16" spacing="1" tilecount="6" columns="2">
+ <image source="grass_dirt_tileset.png" width="33" height="50"/>
+</tileset>
