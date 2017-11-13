29ef447d302caf73e4239b0679d06dca4dabed30
Johannes Mols <johannes.mols@googlemail.com>
2017-11-13 10:22:18 +0100

    The game now recognizes the properties of a collided object, useful to get the weapon name out of an object in the weapon layer

--- core/src/itcom/gangstersquirrel/Objects/InteractiveMapTileObject.java
+++ core/src/itcom/gangstersquirrel/Objects/InteractiveMapTileObject.java
@@ -1,8 +1,13 @@
 package itcom.gangstersquirrel.Objects;
 
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.MapObjects;
+import com.badlogic.gdx.maps.MapProperties;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.maps.tiled.objects.TiledMapTileMapObject;
+import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.*;
 import itcom.gangstersquirrel.MainGameClass;
@@ -13,6 +18,7 @@ import itcom.gangstersquirrel.Screens.PlayScreen;
  */
 public abstract class InteractiveMapTileObject {
 
+    protected PlayScreen playScreen;
     protected World world;
     protected TiledMap map;
     protected TiledMapTile tile;
@@ -21,6 +27,7 @@ public abstract class InteractiveMapTileObject {
     protected Fixture fixture;
 
     public InteractiveMapTileObject(PlayScreen screen, Rectangle bounds, boolean isSensor) {
+        this.playScreen = screen;
         this.world = screen.getWorld();
         this.map = screen.getMap();
         this.bounds = bounds;
@@ -42,11 +49,56 @@ public abstract class InteractiveMapTileObject {
     public abstract void onPlayerBeginContact();
     public abstract void onPlayerEndContact();
 
-    public TiledMapTileLayer.Cell getCell(int layerIndex) {
+    /**
+     * Gets the cell of a graphical map layer
+     * @param layerIndex the index of the map layer
+     * @return the cell
+     */
+    protected TiledMapTileLayer.Cell getCell(int layerIndex) {
         TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(layerIndex);
         return layer.getCell(
                 (int)(body.getPosition().x * MainGameClass.PPM / MainGameClass.TILE_PIXEL_SIZE),
                 (int)(body.getPosition().y * MainGameClass.PPM / MainGameClass.TILE_PIXEL_SIZE)
         );
+    }
+
+    /**
+     * This method returns the properties of an object in a collision layer by checking the player rectangle and object rectangle for an intersection
+     * @param layerIndex the index of the layer in which to search for objects
+     * @return the collided object
+     */
+    protected MapObject getCellProperties(int layerIndex) {
+        MapObjects mapObjects = map.getLayers().get(layerIndex).getObjects();
+
+        for (MapObject mapObject : mapObjects) {
+            MapProperties mapProperties = mapObject.getProperties();
+
+            float width, height, x, y;
+            Rectangle objectRectangle = new Rectangle();
+            Rectangle playerRectangle = new Rectangle();
+
+            if (mapProperties.containsKey("width") && mapProperties.containsKey("height") && mapProperties.containsKey("x") && mapProperties.containsKey("y")) {
+                width = (float) mapProperties.get("width");
+                height = (float) mapProperties.get("height");
+                x = (float) mapProperties.get("x");
+                y = (float) mapProperties.get("y");
+                objectRectangle.set(x, y, width, height);
+            }
+
+            playerRectangle.set(
+                    playScreen.getPlayer().getX() * MainGameClass.PPM,
+                    playScreen.getPlayer().getY() * MainGameClass.PPM,
+                    playScreen.getPlayer().getWidth() * MainGameClass.PPM,
+                    playScreen.getPlayer().getHeight() * MainGameClass.PPM
+            );
+
+            // If the player rectangle and the object rectangle is colliding, return the object
+            if (Intersector.overlaps(objectRectangle, playerRectangle)) {
+                return mapObject;
+            }
+        }
+
+        // If no colliding object was found in that layer
+        return null;
     }
 }
--- core/src/itcom/gangstersquirrel/Objects/MapObjects/WeaponPickup.java
+++ core/src/itcom/gangstersquirrel/Objects/MapObjects/WeaponPickup.java
@@ -1,9 +1,7 @@
 package itcom.gangstersquirrel.Objects.MapObjects;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.maps.MapLayers;
 import com.badlogic.gdx.maps.MapObject;
-import com.badlogic.gdx.maps.MapObjects;
 import com.badlogic.gdx.math.Rectangle;
 import itcom.gangstersquirrel.Items.WeaponList;
 import itcom.gangstersquirrel.Items.WeaponObject;
@@ -30,76 +28,76 @@ public class WeaponPickup extends InteractiveMapTileObject {
         // Remove texture
         getCell(map.getLayers().getIndex("graphics")).setTile(null);
 
-        // TODO: Identify the collided object, at the moment it just looks if there is any object with a weapon property
+        // Add the picked up weapon
+        MapObject pickedUpWeapon = getCellProperties(map.getLayers().getIndex("weapon"));
+        if (pickedUpWeapon.getProperties().containsKey("weapon_name")) {
+            String weaponName = pickedUpWeapon.getProperties().get("weapon_name", String.class);
+            Gdx.app.log("Picked up weapon", weaponName);
 
-        MapObjects objects = playScreen.getMap().getLayers().get("weapon").getObjects();
-        for (MapObject object : objects) {
-            if (object.getProperties().containsKey("weapon_name")) {
-                String weaponName = object.getProperties().get("weapon_name", String.class);
-                Gdx.app.log("Picked up weapon", weaponName);
-
-                boolean changedWeaponList = false;
-                ArrayList<WeaponObject> playerWeaponList = playScreen.getPlayer().getWeapons();
-                WeaponObject weapon;
-                switch (weaponName) {
-                    case "Fists":
-                        weapon = getWeaponObjectByName("Fists");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    case "Branch":
-                        weapon = getWeaponObjectByName("Branch");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    case "Swiss Army Knife":
-                        weapon = getWeaponObjectByName("Swiss Army Knife");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    case "Switchblade":
-                        weapon = getWeaponObjectByName("Switchblade");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    case "Katana":
-                        weapon = getWeaponObjectByName("Katana");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    case "Tommy Gun":
-                        weapon = getWeaponObjectByName("Tommy Gun");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    case "Bazooka":
-                        weapon = getWeaponObjectByName("Bazooka");
-                        if (weapon != null && !weaponAlreadyInList(weapon)) {
-                            playerWeaponList.add(weapon);
-                            changedWeaponList = true;
-                        }
-                        break;
-                    default:
-                        break;
-                }
-
-                if (changedWeaponList) {
-                    playScreen.getPlayer().setWeapons(playerWeaponList);
-                    playScreen.getPlayer().changeWeapon(weaponName);
-                }
+            boolean changedWeaponList = false;
+            ArrayList<WeaponObject> playerWeaponList = playScreen.getPlayer().getWeapons();
+            WeaponObject weapon;
+            switch (weaponName) {
+                case "Fists":
+                    weapon = getWeaponObjectByName("Fists");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                case "Branch":
+                    weapon = getWeaponObjectByName("Branch");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                case "Swiss Army Knife":
+                    weapon = getWeaponObjectByName("Swiss Army Knife");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                case "Switchblade":
+                    weapon = getWeaponObjectByName("Switchblade");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                case "Katana":
+                    weapon = getWeaponObjectByName("Katana");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                case "Tommy Gun":
+                    weapon = getWeaponObjectByName("Tommy Gun");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                case "Bazooka":
+                    weapon = getWeaponObjectByName("Bazooka");
+                    if (weapon != null && !weaponAlreadyInList(weapon)) {
+                        playerWeaponList.add(weapon);
+                        changedWeaponList = true;
+                    }
+                    break;
+                default:
+                    Gdx.app.log("Undefined", "Weapon not defined");
+                    break;
             }
+
+            if (changedWeaponList) {
+                playScreen.getPlayer().setWeapons(playerWeaponList);
+                playScreen.getPlayer().changeWeapon(weaponName);
+            }
+        } else {
+            Gdx.app.log("Not a weapon", "Collided object not a weapon");
         }
     }
 
