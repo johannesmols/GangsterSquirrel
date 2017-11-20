a60074a881805aa724d1c34fa67058374cf56eb0
Johannes Mols <johannes.mols@googlemail.com>
2017-11-21 00:09:31 +0100

    Added a display for the remaining lifes

--- core/src/itcom/gangstersquirrel/Scenes/PlayScreenHud.java
+++ core/src/itcom/gangstersquirrel/Scenes/PlayScreenHud.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import itcom.gangstersquirrel.MainGameClass;
@@ -28,6 +25,7 @@ public class PlayScreenHud {
 
     private Label timerLabel;
     private ProgressBar healthBar;
+    private ImageTextButton lifesImageTextButton;
 
     public PlayScreenHud(PlayScreen playScreen) {
         this.playScreen = playScreen;
@@ -55,10 +53,14 @@ public class PlayScreenHud {
                 "default-horizontal"
         );
 
-        layoutTable.add(healthBar).expandX().growX().left().top();
-        layoutTable.add(new Actor()).expandX().fillX().center().top();
-        layoutTable.add(timerLabel).expandX().right().top();
+        lifesImageTextButton = new ImageTextButton(String.valueOf(playScreen.getGameProgress().getPlayerLifes()), skin, "lifes");
+        lifesImageTextButton.setStyle(changeImageTextButtonStyle(lifesImageTextButton.getStyle(), "fonts/PressStart2P.ttf", 48));
+
+        layoutTable.add(healthBar).expandX().growX().left().top().spaceBottom(25);
+        layoutTable.add(new Actor()).expandX().fillX().center().top().spaceBottom(25);
+        layoutTable.add(timerLabel).expandX().right().top().spaceBottom(25);
         layoutTable.row();
+        layoutTable.add(lifesImageTextButton).expandX().left().top();
 
         stage.addActor(layoutTable);
     }
@@ -66,6 +68,7 @@ public class PlayScreenHud {
     public void update(float deltaTime) {
         timerLabel.setText(String.format("%04d", playScreen.getTimer()));
         healthBar.setValue(playScreen.getPlayer().getHealth());
+        lifesImageTextButton.setText(String.valueOf(playScreen.getGameProgress().getPlayerLifes()));
 
         // Toggle debug
         stage.setDebugAll(MainGameClass.DEBUG);
@@ -76,6 +79,11 @@ public class PlayScreenHud {
     }
 
     private Label.LabelStyle changeLabelStyleFont(Label.LabelStyle original, String filePath, float densityIndependentPixels) {
+        original.font = BitmapFontGenerator.generateFont(filePath, densityIndependentPixels);
+        return original;
+    }
+
+    private ImageTextButton.ImageTextButtonStyle changeImageTextButtonStyle(ImageTextButton.ImageTextButtonStyle original, String filePath, float densityIndependentPixels) {
         original.font = BitmapFontGenerator.generateFont(filePath, densityIndependentPixels);
         return original;
     }
