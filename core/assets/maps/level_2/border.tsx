f3e84d9f95124fa557aa5e372bfb02f4b5f54148
Muheb.moshfiq <mohibkhan@hotmail.com>
2017-12-12 10:08:27 +0100

    Added map level 2

---
+++ core/assets/maps/level_2/border.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="border" tilewidth="16" tileheight="16" tilecount="121" columns="11">
+ <image source="border.PNG" width="183" height="185"/>
+</tileset>
--- core/assets/maps/level_2/ladder.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="16x16-tiles_0" tilewidth="16" tileheight="16" margin="5" tilecount="35" columns="7">
- <image source="16x16-tiles_0.png" width="120" height="86"/>
-</tileset>
