ff8915894548a949e2e27d68d04d09ed98ea64c2
johannesmols <johannes.mols@googlemail.com>
2017-12-17 17:53:01 +0100

    Created abstract menu class and made the level selection screen extend it

--- core/src/itcom/gangstersquirrel/Screens/LevelSelectionMenu.java
+++ core/src/itcom/gangstersquirrel/Screens/LevelSelectionMenu.java
@@ -1,24 +1,13 @@
 package itcom.gangstersquirrel.Screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
-import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
-import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import itcom.gangstersquirrel.GameProgress.GameProgress;
 import itcom.gangstersquirrel.MainGameClass;
 
-public class LevelSelectionMenu implements Screen {
-
-    private MainGameClass game;
-
-    private Skin skin;
-    private Stage stage;
+public class LevelSelectionMenu extends MenuScreen {
 
     private GameProgress gameProgress;
 
@@ -29,29 +18,9 @@ public class LevelSelectionMenu implements Screen {
     private TextButton backButton;
 
     public LevelSelectionMenu(MainGameClass game) {
-        this.game = game;
+        super(game);
+
         gameProgress = new GameProgress();
-    }
-
-    @Override
-    public void show() {
-        stage = new Stage(new ScreenViewport());
-        Gdx.input.setInputProcessor(stage);
-
-        skin = new Skin(Gdx.files.internal("skins/Flat_Earth_UI_Skin/flatearthui/flat-earth-ui.json"));
-
-        ProgressBar.ProgressBarStyle progressBarStyle = skin.get("fancy", ProgressBar.ProgressBarStyle.class);
-        TiledDrawable tiledDrawable = skin.getTiledDrawable("slider-fancy-knob").tint(skin.getColor("selection"));
-        tiledDrawable.setMinWidth(0);
-        progressBarStyle.knobBefore = tiledDrawable;
-
-        Slider.SliderStyle sliderStyle = skin.get("fancy", Slider.SliderStyle.class);
-        sliderStyle.knobBefore = tiledDrawable;
-
-        Table layoutTable = new Table();
-        layoutTable.top();
-        layoutTable.setFillParent(true);
-        layoutTable.pad(getPixelSizeFromDensityIndependentPixels(50));
 
         titleLabel = new Label("Level Selection", skin, "title");
         level1Button = new TextButton("Level 1", skin, "default");
@@ -100,48 +69,6 @@ public class LevelSelectionMenu implements Screen {
         level2Button.addListener(level2ButtonClickListener);
         level3Button.addListener(level3ButtonClickListener);
         backButton.addListener(backButtonClickListener);
-    }
-
-    @Override
-    public void render(float delta) {
-        Gdx.gl.glClearColor(244.0f / 255.0f, 200.0f / 255.0f, 117.0f / 255.0f, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        stage.act(Gdx.graphics.getRawDeltaTime());
-        stage.draw();
-
-        // Toggle debug
-        stage.setDebugAll(MainGameClass.DEBUG);
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        stage.getViewport().update(width, height, true);
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void hide() {
-
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-        skin.dispose();
-    }
-
-    private int getPixelSizeFromDensityIndependentPixels(float dip) {
-        return (int) (dip * Gdx.graphics.getDensity());
     }
 
     /* ----- EVENT LISTENER ----- */
---
+++ core/src/itcom/gangstersquirrel/Screens/MenuScreen.java
@@ -0,0 +1,94 @@
+package itcom.gangstersquirrel.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Slider;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.TiledDrawable;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import itcom.gangstersquirrel.MainGameClass;
+
+public abstract class MenuScreen implements Screen {
+
+    protected MainGameClass game;
+
+    protected Skin skin;
+    protected Stage stage;
+    protected Table layoutTable;
+
+    protected ProgressBar.ProgressBarStyle progressBarStyle;
+    protected Slider.SliderStyle sliderStyle;
+
+    public MenuScreen(MainGameClass game) {
+        this.game = game;
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        skin = new Skin(Gdx.files.internal("skins/Flat_Earth_UI_Skin/flatearthui/flat-earth-ui.json"));
+
+        progressBarStyle = skin.get("fancy", ProgressBar.ProgressBarStyle.class);
+        TiledDrawable tiledDrawable = skin.getTiledDrawable("slider-fancy-knob").tint(skin.getColor("selection"));
+        tiledDrawable.setMinWidth(0);
+        progressBarStyle.knobBefore = tiledDrawable;
+
+        sliderStyle = skin.get("fancy", Slider.SliderStyle.class);
+        sliderStyle.knobBefore = tiledDrawable;
+
+        layoutTable = new Table();
+        layoutTable.top();
+        layoutTable.setFillParent(true);
+        layoutTable.pad(getPixelSizeFromDensityIndependentPixels(50));
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(244.0f / 255.0f, 200.0f / 255.0f, 117.0f / 255.0f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(Gdx.graphics.getRawDeltaTime());
+        stage.draw();
+
+        // Toggle debug
+        stage.setDebugAll(MainGameClass.DEBUG);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        skin.dispose();
+    }
+
+    protected int getPixelSizeFromDensityIndependentPixels(float dip) {
+        return (int) (dip * Gdx.graphics.getDensity());
+    }
+}
