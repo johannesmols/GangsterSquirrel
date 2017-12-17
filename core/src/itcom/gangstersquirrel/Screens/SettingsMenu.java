62b53b93276dc2a3dfb347a5dbe707be5d95180a
johannesmols <johannes.mols@googlemail.com>
2017-12-17 19:03:53 +0100

    Added a checkbox to toggle fullscreen mode in the settings screen

--- core/src/itcom/gangstersquirrel/Screens/SettingsMenu.java
+++ core/src/itcom/gangstersquirrel/Screens/SettingsMenu.java
@@ -1,12 +1,16 @@
 package itcom.gangstersquirrel.Screens;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.CheckBox;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import itcom.gangstersquirrel.MainGameClass;
 import itcom.gangstersquirrel.Settings.Settings;
+import itcom.gangstersquirrel.Tools.BitmapFontGenerator;
 
 public class SettingsMenu extends MenuScreen {
 
@@ -15,6 +19,8 @@ public class SettingsMenu extends MenuScreen {
     private Label titleLabel;
     private TextButton backButton;
 
+    private CheckBox fullscreenCheckBox;
+
     public SettingsMenu(MainGameClass game) {
         super(game);
         this.settings = new Settings();
@@ -22,7 +28,14 @@ public class SettingsMenu extends MenuScreen {
         titleLabel = new Label("Settings", skin, "title");
         backButton = new TextButton("Back", skin, "default");
 
+        fullscreenCheckBox = new CheckBox(" Fullscreen", skin, "default");
+        fullscreenCheckBox.setChecked(settings.getFullscreen());
+
         layoutTable.add(titleLabel).top().center().expandX().colspan(3).spaceBottom(getPixelSizeFromDensityIndependentPixels(25f));
+        layoutTable.row();
+        layoutTable.add(new Actor()).expandX().spaceBottom(getPixelSizeFromDensityIndependentPixels(25f));
+        layoutTable.add(fullscreenCheckBox).top().center().growX().expandX().spaceBottom(getPixelSizeFromDensityIndependentPixels(25f));
+        layoutTable.add(new Actor()).expandX().spaceBottom(getPixelSizeFromDensityIndependentPixels(25f));
         layoutTable.row();
         layoutTable.add(new Actor()).expandX().spaceBottom(getPixelSizeFromDensityIndependentPixels(25f));
         layoutTable.add(backButton).top().center().growX().expandX().spaceBottom(getPixelSizeFromDensityIndependentPixels(25f));
@@ -31,6 +44,7 @@ public class SettingsMenu extends MenuScreen {
 
         stage.addActor(layoutTable);
 
+        fullscreenCheckBox.addListener(fullscreenCheckBoxChangeListener);
         backButton.addListener(backButtonClickListener);
     }
 
@@ -39,7 +53,20 @@ public class SettingsMenu extends MenuScreen {
 
     }
 
+    private Label.LabelStyle changeLabelStyleFont(Label.LabelStyle original, String filePath, float densityIndependentPixels, Color color) {
+        original.font = BitmapFontGenerator.generateFont(filePath, densityIndependentPixels, color);
+        return original;
+    }
+
     /* ----- EVENT LISTENER ----- */
+
+    private ChangeListener fullscreenCheckBoxChangeListener = new ChangeListener() {
+        @Override
+        public void changed(ChangeEvent event, Actor actor) {
+            settings.setFullscreen(fullscreenCheckBox.isChecked());
+            game.changeGameResolution();
+        }
+    };
 
     private ClickListener backButtonClickListener = new ClickListener() {
         @Override
